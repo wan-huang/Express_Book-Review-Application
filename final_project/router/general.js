3572bc84dd53e5d2b6dadfac9e454f7c6e5152b0
wan-huang <wan.huang@mail.mcgill.ca>
2023-06-08 21:08:42 -0400

    Task 6-9 Implemented POST, PUT, DELETE method for general and authenticated user routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,18 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
